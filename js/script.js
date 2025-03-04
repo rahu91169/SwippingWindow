4cd217902b2f9bc5f74f342732a5bd60bde9453a
Rahul Sharma <rahul91169@gmail.com>
2025-03-04 23:00:21 +0530

    changes related to swipper windows

---
+++ js/script.js
@@ -0,0 +1,36 @@
+var swiper = new Swiper(".slide-container", {
+    slidesPerView: 4,
+    spaceBetween: 20,
+    sliderPerGroup: 4,
+    loop: true,
+    centerSlide: "true",
+    fade: "true",
+    grabCursor: "true",
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    autoplay: {
+        delay: 1000, // Auto slide every 3 seconds
+        disableOnInteraction: false, // Keeps autoplay running even after user interaction
+      },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      520: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 3,
+      },
+      1000: {
+        slidesPerView: 4,
+      },
+    },
+  });
